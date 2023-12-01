9808c2c128728b68747d575c50127f6e4fa6b38b
Dainis Berzins <db5331@gmail.com>
2023-12-01 22:26:39 +0200

    Publishing models

--- sarakste_app/admin.py
+++ sarakste_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Segment)
+admin.site.register(Summary)
+admin.site.register(Snippet)
+admin.site.register(UserSnippet)
