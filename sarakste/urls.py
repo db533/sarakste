4ed19415a26105502c353ab33f06aec8d0624c72
Dainis Berzins <db5331@gmail.com>
2023-12-02 20:19:59 +0200

    Fixing login v0.3

--- sarakste/urls.py
+++ sarakste/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from sarakste_app.views import *
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
