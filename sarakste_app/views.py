7c5bbd7051233d33cc37bf6d6a355781f289430b
Dainis Berzins <db5331@gmail.com>
2023-12-03 17:58:48 +0200

    Adding data to lasit view v0.7

--- sarakste_app/views.py
+++ sarakste_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Snippet, UserSnippet
+from .models import *
 from django.contrib.auth.decorators import login_required
 from django.db.models import F
 
