3193472b3bded235b9606d04f34d5c9aee13123a
Dainis Berzins <db5331@gmail.com>
2023-12-21 19:17:53 +0200

    Trying to fix ascii error when entering search phrase v0.4

--- sarakste/settings.py
+++ sarakste/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 import os
+import sys
 import io
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
